0664dbcbdc3cbf6f318616349b1c5c970f739712
civisrom <167646351+civisrom@users.noreply.github.com>
2024-11-22 22:58:17 +0300

    Update android_time_fixer.py

--- android_time_fixer.py
+++ android_time_fixer.py
@@ -9,6 +9,7 @@ import json
 import subprocess
 import ntplib
 from pathlib import Path
+from androguard.core.bytecodes import apk
 import pyperclip
 import colorama
 from colorama import Fore, Style, init
