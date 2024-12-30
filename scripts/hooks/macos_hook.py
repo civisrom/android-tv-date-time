03d210b007c977aa05316c0176e9ee486017ec3b
civisrom <civisromanus65@gmail.com>
2024-12-30 23:37:48 +0300

    modified:   scripts/hooks/linux_hook.py 	modified:   scripts/hooks/macos_hook.py 	modified:   scripts/hooks/win_hook.py

--- scripts/hooks/macos_hook.py
+++ scripts/hooks/macos_hook.py
@@ -1,7 +1,12 @@
-# scripts/hooks/macos_hook.py
 import os
 import sys
 
 if sys.platform == 'darwin':
-    if hasattr(sys, '_MEIPASS'):
-        os.environ['PATH'] = os.path.dirname(sys._MEIPASS) + os.pathsep + os.environ['PATH']
+    try:
+        if hasattr(sys, '_MEIPASS'):
+            if 'PATH' in os.environ:
+                os.environ['PATH'] = os.path.join(sys._MEIPASS) + os.pathsep + os.environ['PATH']
+            else:
+                os.environ['PATH'] = os.path.join(sys._MEIPASS)
+    except Exception as e:
+        print(f"Error configuring PATH for macOS: {e}")
